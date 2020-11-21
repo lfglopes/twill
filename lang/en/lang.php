70817def6b972f62188d93807c8927d5b2b71fab
Luis Lopes <luis@bitok.pt>
2020-11-21 12:43:58 +0100

    Make parts of settings & dashboard translatable

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -42,6 +42,18 @@ return [
         'my-drafts' => 'My drafts',
         'search-placeholder' => 'Search everything...',
         'statitics' => 'Statistics',
+        'activities' => [
+            'created' => 'Created',
+            'updated' => 'Updated',
+            'unpublished' => 'Unpublished',
+            'published' => 'Published'
+        ],
+        'activity-row' => [
+            'edit' => 'Edit',
+            'view-permalink' => 'View Permalink',
+            'by' => 'by'
+        ],
+        'unknown-author' => 'Unknown'
     ],
     'dialog' => [
         'cancel' => 'Cancel',
@@ -222,6 +234,11 @@ return [
         'media-library' => 'Media Library',
         'settings' => 'Settings',
         'close-menu' => 'Close menu',
+    ],
+    'settings' => [
+        'update' => 'Update',
+        'cancel' => 'Cancel',
+        'fieldset-label' => 'Edit settings'
     ],
     'notifications' => [
         'reset' => [
